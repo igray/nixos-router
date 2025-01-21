3e467189fa8abde530e0f8df0e213dfdb95c736e
Iain Gray <igray78756@gmail.com>
2025-01-20 20:59:00 -0600

    Fixed deprecated forward directive

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -103,7 +103,8 @@
           DNSOverTLS = true;
           DNSSEC = true;
           IPv6PrivacyExtensions = false;
-          IPForward = true;
+          IPv4Forwarding = true;
+          IPv6Forwarding = true;
         };
         # make routing on this interface a dependency for network-online.target
         linkConfig.RequiredForOnline = "routable";
